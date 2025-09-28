f59fd20eba565b19e37ec3106bad3718ba2f8c77
Matthew Duarte <hello.codehipster@outlook.com>
2025-09-28 18:11:38 -0500

    update

--- sist. notificaciones/notificacion.php
+++ sist. notificaciones/notificacion.php
@@ -1,7 +1,7 @@
 <?php
 function enviarNotificacion($mensaje, $destinatario) {
     // Aquí iría la lógica para enviar la notificación (email, SMS, etc.)
-    $fecha = date()"Y-m-d H:i:s");
+    $fecha = date()("Y-m-d H:i:s");
     $estado = "enviada";
 
     //Conectarse a la base de datos (suponiendo que ya tienes una conexión establecida)
